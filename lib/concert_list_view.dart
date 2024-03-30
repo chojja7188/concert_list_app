d7860efa83317c90625625aec52e1e39593f9c70
chojja7188 <shoun7188@naver.com>
2024-03-30 17:29:37 +0900

    feat: 기본 구성 초안

---
+++ lib/concert_list_view.dart
@@ -0,0 +1,51 @@
+import 'package:concert_list_app/concert_box.dart';
+import 'package:flutter/material.dart';
+
+class ConcertListView extends StatefulWidget {
+  const ConcertListView({super.key});
+
+  @override
+  State<ConcertListView> createState() => _ConcertListViewState();
+}
+
+class _ConcertListViewState extends State<ConcertListView> {
+  @override
+  Widget build(BuildContext context) {
+    return const SingleChildScrollView(
+      child: Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.only(bottom: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ConcertBox(),
+                ConcertBox()
+              ],
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(bottom: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ConcertBox(),
+                ConcertBox()
+              ],
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(bottom: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ConcertBox(),
+                ConcertBox()
+              ],
+            ),
+          ),
+        ],
+      )
+    );
+  }
+}
