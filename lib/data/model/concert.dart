d07d05293d0c8510649abe0ee272cff0e6c988db
chojja7188 <shoun7188@naver.com>
2024-04-10 23:16:21 +0900

    feat: Concert 모델 작성

---
+++ lib/data/model/concert.dart
@@ -0,0 +1,31 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'concert.freezed.dart';
+
+part 'concert.g.dart';
+
+@freezed
+class Concert with _$Concert {
+  const factory Concert({
+    @JsonKey(name: 'mt20id')
+    required String id,
+    @JsonKey(name: 'prfnm')
+    required String name,
+    @JsonKey(name: 'genrenm')
+    required String genre,
+    @JsonKey(name: 'prfstate')
+    required String state,
+    @JsonKey(name: 'prfpdform')
+    required String startAt,
+    @JsonKey(name: 'prfpdto')
+    required String endAt,
+    @JsonKey(name: 'poster')
+    required String posterPath,
+    @JsonKey(name: 'fcltynm')
+    required String stage,
+    required String openrun,
+    required String area
+  }) = _Concert;
+
+  factory Concert.fromJson(Map<String, Object?> json) => _$ConcertFromJson(json);
+}
---
+++ lib/data/model/concert.freezed.dart
@@ -0,0 +1,357 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'concert.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models');
+
+Concert _$ConcertFromJson(Map<String, dynamic> json) {
+  return _Concert.fromJson(json);
+}
+
+/// @nodoc
+mixin _$Concert {
+  @JsonKey(name: 'mt20id')
+  String get id => throw _privateConstructorUsedError;
+  @JsonKey(name: 'prfnm')
+  String get name => throw _privateConstructorUsedError;
+  @JsonKey(name: 'genrenm')
+  String get genre => throw _privateConstructorUsedError;
+  @JsonKey(name: 'prfstate')
+  String get state => throw _privateConstructorUsedError;
+  @JsonKey(name: 'prfpdform')
+  String get startAt => throw _privateConstructorUsedError;
+  @JsonKey(name: 'prfpdto')
+  String get endAt => throw _privateConstructorUsedError;
+  @JsonKey(name: 'poster')
+  String get posterPath => throw _privateConstructorUsedError;
+  @JsonKey(name: 'fcltynm')
+  String get stage => throw _privateConstructorUsedError;
+  String get openrun => throw _privateConstructorUsedError;
+  String get area => throw _privateConstructorUsedError;
+
+  Map<String, dynamic> toJson() => throw _privateConstructorUsedError;
+  @JsonKey(ignore: true)
+  $ConcertCopyWith<Concert> get copyWith => throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $ConcertCopyWith<$Res> {
+  factory $ConcertCopyWith(Concert value, $Res Function(Concert) then) =
+      _$ConcertCopyWithImpl<$Res, Concert>;
+  @useResult
+  $Res call(
+      {@JsonKey(name: 'mt20id') String id,
+      @JsonKey(name: 'prfnm') String name,
+      @JsonKey(name: 'genrenm') String genre,
+      @JsonKey(name: 'prfstate') String state,
+      @JsonKey(name: 'prfpdform') String startAt,
+      @JsonKey(name: 'prfpdto') String endAt,
+      @JsonKey(name: 'poster') String posterPath,
+      @JsonKey(name: 'fcltynm') String stage,
+      String openrun,
+      String area});
+}
+
+/// @nodoc
+class _$ConcertCopyWithImpl<$Res, $Val extends Concert>
+    implements $ConcertCopyWith<$Res> {
+  _$ConcertCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? id = null,
+    Object? name = null,
+    Object? genre = null,
+    Object? state = null,
+    Object? startAt = null,
+    Object? endAt = null,
+    Object? posterPath = null,
+    Object? stage = null,
+    Object? openrun = null,
+    Object? area = null,
+  }) {
+    return _then(_value.copyWith(
+      id: null == id
+          ? _value.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as String,
+      name: null == name
+          ? _value.name
+          : name // ignore: cast_nullable_to_non_nullable
+              as String,
+      genre: null == genre
+          ? _value.genre
+          : genre // ignore: cast_nullable_to_non_nullable
+              as String,
+      state: null == state
+          ? _value.state
+          : state // ignore: cast_nullable_to_non_nullable
+              as String,
+      startAt: null == startAt
+          ? _value.startAt
+          : startAt // ignore: cast_nullable_to_non_nullable
+              as String,
+      endAt: null == endAt
+          ? _value.endAt
+          : endAt // ignore: cast_nullable_to_non_nullable
+              as String,
+      posterPath: null == posterPath
+          ? _value.posterPath
+          : posterPath // ignore: cast_nullable_to_non_nullable
+              as String,
+      stage: null == stage
+          ? _value.stage
+          : stage // ignore: cast_nullable_to_non_nullable
+              as String,
+      openrun: null == openrun
+          ? _value.openrun
+          : openrun // ignore: cast_nullable_to_non_nullable
+              as String,
+      area: null == area
+          ? _value.area
+          : area // ignore: cast_nullable_to_non_nullable
+              as String,
+    ) as $Val);
+  }
+}
+
+/// @nodoc
+abstract class _$$ConcertImplCopyWith<$Res> implements $ConcertCopyWith<$Res> {
+  factory _$$ConcertImplCopyWith(
+          _$ConcertImpl value, $Res Function(_$ConcertImpl) then) =
+      __$$ConcertImplCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call(
+      {@JsonKey(name: 'mt20id') String id,
+      @JsonKey(name: 'prfnm') String name,
+      @JsonKey(name: 'genrenm') String genre,
+      @JsonKey(name: 'prfstate') String state,
+      @JsonKey(name: 'prfpdform') String startAt,
+      @JsonKey(name: 'prfpdto') String endAt,
+      @JsonKey(name: 'poster') String posterPath,
+      @JsonKey(name: 'fcltynm') String stage,
+      String openrun,
+      String area});
+}
+
+/// @nodoc
+class __$$ConcertImplCopyWithImpl<$Res>
+    extends _$ConcertCopyWithImpl<$Res, _$ConcertImpl>
+    implements _$$ConcertImplCopyWith<$Res> {
+  __$$ConcertImplCopyWithImpl(
+      _$ConcertImpl _value, $Res Function(_$ConcertImpl) _then)
+      : super(_value, _then);
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? id = null,
+    Object? name = null,
+    Object? genre = null,
+    Object? state = null,
+    Object? startAt = null,
+    Object? endAt = null,
+    Object? posterPath = null,
+    Object? stage = null,
+    Object? openrun = null,
+    Object? area = null,
+  }) {
+    return _then(_$ConcertImpl(
+      id: null == id
+          ? _value.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as String,
+      name: null == name
+          ? _value.name
+          : name // ignore: cast_nullable_to_non_nullable
+              as String,
+      genre: null == genre
+          ? _value.genre
+          : genre // ignore: cast_nullable_to_non_nullable
+              as String,
+      state: null == state
+          ? _value.state
+          : state // ignore: cast_nullable_to_non_nullable
+              as String,
+      startAt: null == startAt
+          ? _value.startAt
+          : startAt // ignore: cast_nullable_to_non_nullable
+              as String,
+      endAt: null == endAt
+          ? _value.endAt
+          : endAt // ignore: cast_nullable_to_non_nullable
+              as String,
+      posterPath: null == posterPath
+          ? _value.posterPath
+          : posterPath // ignore: cast_nullable_to_non_nullable
+              as String,
+      stage: null == stage
+          ? _value.stage
+          : stage // ignore: cast_nullable_to_non_nullable
+              as String,
+      openrun: null == openrun
+          ? _value.openrun
+          : openrun // ignore: cast_nullable_to_non_nullable
+              as String,
+      area: null == area
+          ? _value.area
+          : area // ignore: cast_nullable_to_non_nullable
+              as String,
+    ));
+  }
+}
+
+/// @nodoc
+@JsonSerializable()
+class _$ConcertImpl implements _Concert {
+  const _$ConcertImpl(
+      {@JsonKey(name: 'mt20id') required this.id,
+      @JsonKey(name: 'prfnm') required this.name,
+      @JsonKey(name: 'genrenm') required this.genre,
+      @JsonKey(name: 'prfstate') required this.state,
+      @JsonKey(name: 'prfpdform') required this.startAt,
+      @JsonKey(name: 'prfpdto') required this.endAt,
+      @JsonKey(name: 'poster') required this.posterPath,
+      @JsonKey(name: 'fcltynm') required this.stage,
+      required this.openrun,
+      required this.area});
+
+  factory _$ConcertImpl.fromJson(Map<String, dynamic> json) =>
+      _$$ConcertImplFromJson(json);
+
+  @override
+  @JsonKey(name: 'mt20id')
+  final String id;
+  @override
+  @JsonKey(name: 'prfnm')
+  final String name;
+  @override
+  @JsonKey(name: 'genrenm')
+  final String genre;
+  @override
+  @JsonKey(name: 'prfstate')
+  final String state;
+  @override
+  @JsonKey(name: 'prfpdform')
+  final String startAt;
+  @override
+  @JsonKey(name: 'prfpdto')
+  final String endAt;
+  @override
+  @JsonKey(name: 'poster')
+  final String posterPath;
+  @override
+  @JsonKey(name: 'fcltynm')
+  final String stage;
+  @override
+  final String openrun;
+  @override
+  final String area;
+
+  @override
+  String toString() {
+    return 'Concert(id: $id, name: $name, genre: $genre, state: $state, startAt: $startAt, endAt: $endAt, posterPath: $posterPath, stage: $stage, openrun: $openrun, area: $area)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$ConcertImpl &&
+            (identical(other.id, id) || other.id == id) &&
+            (identical(other.name, name) || other.name == name) &&
+            (identical(other.genre, genre) || other.genre == genre) &&
+            (identical(other.state, state) || other.state == state) &&
+            (identical(other.startAt, startAt) || other.startAt == startAt) &&
+            (identical(other.endAt, endAt) || other.endAt == endAt) &&
+            (identical(other.posterPath, posterPath) ||
+                other.posterPath == posterPath) &&
+            (identical(other.stage, stage) || other.stage == stage) &&
+            (identical(other.openrun, openrun) || other.openrun == openrun) &&
+            (identical(other.area, area) || other.area == area));
+  }
+
+  @JsonKey(ignore: true)
+  @override
+  int get hashCode => Object.hash(runtimeType, id, name, genre, state, startAt,
+      endAt, posterPath, stage, openrun, area);
+
+  @JsonKey(ignore: true)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$ConcertImplCopyWith<_$ConcertImpl> get copyWith =>
+      __$$ConcertImplCopyWithImpl<_$ConcertImpl>(this, _$identity);
+
+  @override
+  Map<String, dynamic> toJson() {
+    return _$$ConcertImplToJson(
+      this,
+    );
+  }
+}
+
+abstract class _Concert implements Concert {
+  const factory _Concert(
+      {@JsonKey(name: 'mt20id') required final String id,
+      @JsonKey(name: 'prfnm') required final String name,
+      @JsonKey(name: 'genrenm') required final String genre,
+      @JsonKey(name: 'prfstate') required final String state,
+      @JsonKey(name: 'prfpdform') required final String startAt,
+      @JsonKey(name: 'prfpdto') required final String endAt,
+      @JsonKey(name: 'poster') required final String posterPath,
+      @JsonKey(name: 'fcltynm') required final String stage,
+      required final String openrun,
+      required final String area}) = _$ConcertImpl;
+
+  factory _Concert.fromJson(Map<String, dynamic> json) = _$ConcertImpl.fromJson;
+
+  @override
+  @JsonKey(name: 'mt20id')
+  String get id;
+  @override
+  @JsonKey(name: 'prfnm')
+  String get name;
+  @override
+  @JsonKey(name: 'genrenm')
+  String get genre;
+  @override
+  @JsonKey(name: 'prfstate')
+  String get state;
+  @override
+  @JsonKey(name: 'prfpdform')
+  String get startAt;
+  @override
+  @JsonKey(name: 'prfpdto')
+  String get endAt;
+  @override
+  @JsonKey(name: 'poster')
+  String get posterPath;
+  @override
+  @JsonKey(name: 'fcltynm')
+  String get stage;
+  @override
+  String get openrun;
+  @override
+  String get area;
+  @override
+  @JsonKey(ignore: true)
+  _$$ConcertImplCopyWith<_$ConcertImpl> get copyWith =>
+      throw _privateConstructorUsedError;
+}
