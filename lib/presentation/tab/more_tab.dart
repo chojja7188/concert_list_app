8a204db0e20066e42783789049840a28434f8263
chojja7188 <shoun7188@naver.com>
2024-04-12 17:26:59 +0900

    feat: 더보기탭 서비스 명시 디자인 수정

--- lib/presentation/tab/more_tab.dart
+++ lib/presentation/tab/more_tab.dart
@@ -1,3 +1,4 @@
+import 'package:concert_list_app/config/ui_config.dart';
 import 'package:flutter/material.dart';
 
 class MoreTab extends StatelessWidget {
@@ -9,9 +10,8 @@ class MoreTab extends StatelessWidget {
       padding: EdgeInsets.all(16),
       child: Column(
         children: [
-
           Text('본 서비스는 KOPIS OPEN API를 사용하고 있습니다.',
-          style: TextStyle(color: Colors.grey),
+          style: TextStyle(color: UiConfig.primaryColor),
           ),
           Text("(재)예술경영지원센터 공연예술통합전산망, www.kopis.or.kr",
           style: TextStyle(color: Colors.grey, fontSize: 12))
