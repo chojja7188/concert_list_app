622c3b07f3a7e34b12c06544ce0bc761a0995e14
chojja7188 <shoun7188@naver.com>
2024-05-11 01:14:18 +0900

    fix: 보관된 리스트에 상태가 제대로 보이지 않던 현상 수정

--- lib/presentation/archive/archive_view_model.dart
+++ lib/presentation/archive/archive_view_model.dart
@@ -3,6 +3,7 @@ import 'package:concert_list_app/domain/model/concert_detail.dart';
 import 'package:concert_list_app/domain/repository/concert_repository.dart';
 import 'package:concert_list_app/domain/service/hive_service.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class ArchiveViewModel with ChangeNotifier{
   final ConcertRepository _concertRepository;
@@ -12,7 +13,19 @@ class ArchiveViewModel with ChangeNotifier{
   List<ConcertDetail> get archiveConcertList => List.unmodifiable(_archiveConcertList);
 
   void fetchArchivedConcert() async {
-    _archiveConcertList = await HiveService().getArchivedConcertList();
+    final hiveArchiveConcertList = await HiveService().getArchivedConcertList();
+    _archiveConcertList = hiveArchiveConcertList.map((e) => e.copyWith(state: _setState(startDate: e.startAt, endDate: e.endAt))).toList();
     notifyListeners();
+  }
+
+  String _setState({required String startDate, required String endDate}) {
+    final int nowDateNum = int.parse(DateFormat('yyyyMMdd').format(DateTime.now()));
+    final int startDateNum = int.parse(startDate.replaceAll('.', ''));
+    final int endDateNum = int.parse(endDate.replaceAll('.', ''));
+
+    if (startDateNum > nowDateNum) return '공연예정';
+    if (startDateNum <= nowDateNum && nowDateNum <= endDateNum) return '공연중';
+    if (endDateNum < nowDateNum) return '공연완료';
+    return '';
   }
 }
